4a96d3cd604796fd5627bd73c7128264df100410
sei <teatime.0704@gmail.com>
2020-04-14 23:01:03 +0900

    [add] route + controller

--- app/Http/Controllers/CrudController.php
+++ app/Http/Controllers/CrudController.php
@@ -6,5 +6,28 @@ use Illuminate\Http\Request;
 
 class CrudController extends Controller
 {
+    public function index(){
+        return view('index');
+    }
+    public function show( $id = 0 ){
+        return view('detail');
+    }
     
+    public function create(){
+        return view('create');
+    }
+    public function new(){
+        // return redirect('');
+    }
+
+    public function edit( $id = 0 ){
+        return view('edit');
+    }
+    public function update(){
+        // return redirect('');
+    }
+
+    public function delete( $id = 0 ){
+        // return redirect('');
+    }
 }
